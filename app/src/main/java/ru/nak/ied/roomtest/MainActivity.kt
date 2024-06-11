c789e0186eb64ac421ddc5e8f4eb44661ab32aed
Aleksey Bershov <bershov.aleksey@bk.ru>
2024-06-11 08:48:39 +0300

    refactor onCreate

--- app/src/main/java/ru/nak/ied/roomtest/MainActivity.kt
+++ app/src/main/java/ru/nak/ied/roomtest/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.asLiveData
 import ru.nak.ied.roomtest.databinding.ActivityMainBinding
 import java.util.concurrent.Executors
 
@@ -17,14 +18,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val db = MainDb.getDb(this)
+        db.getDao().getAllItem().asLiveData().observe(this) {list ->
+            // каждый раз что бы не добавлялость лишнее будем очищать наш список на экране
+            binding.tvlist.text = ""
+            list.forEach {
+                val text = "Id: ${it.id} Name: ${it.name} Prise: ${it.price}\n"
+                binding.tvlist.append(text)
+            }
+        }
 
-        binding.button.setOnClickListener{
-            val item = Item(null,
+        binding.button.setOnClickListener {
+            val item = Item(
+                null,
                 binding.edName.text.toString(),
                 binding.edPrice.text.toString()
             )
+
+//            есть просто вариант с новым потоком
+//            Thread {
+//                db.getDao().insertItem(item)
+//            }.start()
+
             val executor = Executors.newSingleThreadExecutor()
-            executor.execute{
+            executor.execute {
                 db.getDao().insertItem(item)
             }
             executor.shutdown()
